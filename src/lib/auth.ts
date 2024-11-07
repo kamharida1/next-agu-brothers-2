f36c7e3439f74758c6774f278350f1fc8356f952
kamharida1 <kamharida007@gmail.com>
2024-11-07 11:09:08 +0100

    facebook auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,12 +4,17 @@ import dbConnect from "./dbConnect";
 import UserModel from "./models/UserModel";
 import bcrypt from "bcryptjs";
 import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook"
 
 export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET
     }),
     CredentialsProvider({
       credentials: {
